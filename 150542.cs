45c41e41efbf4467a6d4e8956382215669fc079e
Orzoch <47740876+kziel445@users.noreply.github.com>
2019-10-18 14:32:52 +0200

    Add files via upload

---
+++ 150542.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double C, F;
+            C = Convert.ToDouble(Console.ReadLine());
+            F = (C * 1.8) + 32;
+            Console.WriteLine(C.ToString() +" "+ F.ToString() );
+            Console.ReadKey();
+        }
+    }
+}
