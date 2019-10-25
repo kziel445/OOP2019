6facbc9247a9c23c57716ba9f3f08ac59d49e993
Orzoch <47740876+kziel445@users.noreply.github.com>
2019-10-25 14:29:45 +0200

    Add files via upload

---
+++ Lesson3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Szkola
+    {
+        public int liczbaUczniow;
+        public int liczbaLawek;
+        public int SprawdzLawki(int uczniowie, int lawki)
+        {
+            if (2 * uczniowie > lawki) return 0;
+            return 1;
+        }
+    }
+}
---
+++ Lesson3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 2
+            Szkola UWM = new Szkola();
+            UWM.liczbaLawek = 40;
+            UWM.liczbaUczniow = 81;
+            int x = UWM.SprawdzLawki(UWM.liczbaUczniow, UWM.liczbaLawek);
+            if (x == 1) Console.WriteLine("Jest wystarczajaco lawek dla uczniow");
+            else Console.WriteLine("Lawek jest za malo");
+        }
+    }
+}
