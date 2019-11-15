f10e9af8c11f8a2caf9885982424b6bd53e65df4
Orzoch <47740876+kziel445@users.noreply.github.com>
2019-11-15 16:14:32 +0100

    Add files via upload

---
+++ Lesson5/Zad5_2/ConsoleApp3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Test
+    {
+        public int liczba;
+        public Test2 poleTestowe = new Test2();
+
+        public Object Clone()
+        {
+            return MemberwiseClone();
+        }
+        public Test GlebokaKopia()
+        {
+            Test tempTest = new Test();
+            tempTest.liczba = this.liczba;
+            tempTest.poleTestowe.slowo = this.poleTestowe.slowo;
+            return tempTest;
+        }
+    }
+    class Test2
+    {
+        public string slowo;
+    }
+
+}
---
+++ Lesson5/Zad5_2/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obiekt1 = new Test();
+            Test obiekt2 = new Test();
+            Test obiekt3 = new Test();
+            Test obiekt4 = new Test();
+
+            obiekt1.liczba = 255; //1 = 255
+            
+            obiekt2 = obiekt1; //obiekt2 jest od teraz rowny obiekt1
+            obiekt3 = (Test)obiekt1.Clone(); //3 = 255
+            obiekt1.liczba = 347; //1 = 347
+            
+            Console.WriteLine(obiekt1.liczba + " " + obiekt2.liczba  + " "+ obiekt3.liczba );
+
+            
+            obiekt1.poleTestowe.slowo = "owo";
+            obiekt1.poleTestowe.slowo = "kaa";
+            Console.WriteLine(obiekt1.poleTestowe.slowo);
+            Console.WriteLine(obiekt1.poleTestowe.slowo);
+            obiekt3 = obiekt1.GlebokaKopia();
+        }
+    }
+    
+    
+    
+}
