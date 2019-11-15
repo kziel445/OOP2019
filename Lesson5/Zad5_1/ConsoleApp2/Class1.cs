df88aad441c57c928c058feb5a62bdd6a90973ec
Orzoch <47740876+kziel445@users.noreply.github.com>
2019-11-15 15:13:39 +0100

    Add files via upload

---
+++ Lesson5/Zad5_1/ConsoleApp2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Vehicle
+    {
+    }
+    class Car : Vehicle , IRideable
+    {
+        public void Ride()
+        {
+            Console.WriteLine("Jadę samochodem");
+        }
+    }
+    class Bicycle : Vehicle , IRideable
+    {
+        public void Ride()
+        {
+            Console.WriteLine("Jadę rowerem");
+        }
+    }
+    class Osoba : ISkrzypek, IGitarzysta
+    {
+        public void Graj()
+        {
+            Console.WriteLine("Gram ale kto wie na czym");
+        }
+        void ISkrzypek.Graj()
+        {
+            Console.WriteLine("Gram na skrzypcach");
+        }
+        void IGitarzysta.Graj()
+        {
+            Console.WriteLine("Gram ma gitarce");
+        }
+    }
+
+
+}
---
+++ Lesson5/Zad5_1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Ride();
+            Bicycle bicycle1 = new Bicycle();
+            bicycle1.Ride();
+            Osoba czlowiek = new Osoba();
+            czlowiek.Graj();
+            IGitarzysta gitarzysta = new Osoba();
+            gitarzysta.Graj();
+            ISkrzypek skrzypek = new Osoba();
+            skrzypek.Graj();
+        }
+    }
+}
