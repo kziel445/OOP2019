dcba445315abd52d496fcc9877e98545f5adbb86
Orzoch <47740876+kziel445@users.noreply.github.com>
2019-12-06 12:06:44 +0100

    Add files via upload

---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie: " + (150542 % 3 + 1));      //  :)
+
+            Katalog Dramat = new Katalog("dramat");
+            Dramat.DodajPozycje(new Ksiazka(1, "Troll", "Miech", 1, 4));
+            Dramat.DodajPozycje(new Czasopismo(1, "Pudelek", "Ogon", 1, 4));
+            Dramat.WypiszWszystko();
+            Console.WriteLine();
+            Dramat.WyszukajPoId(2);
+            Dramat.WyszukajPoTytule("Troll");
+
+
+
+
+        }
+    }
+}
